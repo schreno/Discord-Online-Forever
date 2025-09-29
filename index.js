e4f765fbe40a4744f5a273bd52e324e392f4a893
schreno <154241496+schreno@users.noreply.github.com>
2025-09-30 06:11:42 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,7 @@ const bot = new Eris(process.env.token);
 bot.on("ready", () => {
   console.log(`Connected as ${bot.user.username}#${bot.user.discriminator}`);
 
-  // set status to idle and activity text to "test"
-  // type: 0 = Playing, 1 = Streaming, 2 = Listening, 3 = Watching
-  bot.editStatus("idle", { name: "test", type: 0 });
+  bot.editStatus("dnd");
 });
 
 bot.on("error", (err) => {
