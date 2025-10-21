a1a17885b4341498445dba69efd01cce2e4ea25b
schreno <154241496+schreno@users.noreply.github.com>
2025-10-22 01:55:51 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const bot = new Eris(process.env.token);
 bot.on("ready", () => {
   console.log(`Connected as ${bot.user.username}#${bot.user.discriminator}`);
 
-  bot.editStatus("idle");
+  bot.editStatus("dnd");
 });
 
 bot.on("error", (err) => {
